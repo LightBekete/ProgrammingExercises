9701e3886a8fa92a88747ad65dff24363809c75a
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-16 18:42:16 +0200

    second and final commit of the user subscription using the switch statement.all conditions are coded. comments have been added to condition 3 and 4

--- usersubscription3.cpp
+++ usersubscription3.cpp
@@ -12,17 +12,25 @@
                break;
         case 1: cout<<"Your subscription ends within a day. Renew now and save 20% !"<<endl;
                break;
+
+               //when days is equal between 2 and 5
         case 2:
         case 3:
         case 4:
         case 5:cout<<"Your subscription expires in "<<days<<" days. Renew now and save 10%"<<endl;
                break;
-        /*case 6:
+
+             //when days is equal between 6 and 10
+        case 6:
         case 7:
         case 8:
         case 9:
-        case 10:
-        case 11:*/
+        case 10:cout<<"Your subscription will expire soon. Renew now!"<<endl;
+               break;
+
+               //when days is equal to 11
+        case 11: cout<<"You have an active subscription."<<endl;
+               break;
      }
     return 0;
   }
