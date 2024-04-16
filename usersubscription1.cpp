58a167352ff399f6439daf74b3e5c5ffb133915b
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-16 17:45:23 +0200

    second and final commit of the user subscription using the if statements.the last two conditions are coded. code has been refactored.the literal value 11 has been changed to 12

--- usersubscription1.cpp
+++ usersubscription1.cpp
@@ -6,17 +6,27 @@
      int main(){
         srand(time(0)) ;  //intialising randon seed
 
-       int days = rand() % 11;
-       if (days == 0){
+        int days = rand() % 12; // changed from 11 to 12
+      
+         if (days == 0){
         cout<<"Your subscription has expired"<<endl;
        }
        if (days == 1){
         cout<<"Your subscription ends within a day. Renew now and save 20% !"<<endl;
        }
+    
 
-       if (days <= 5){
+       if (days >=2 && days <= 5){
             cout<<"Your subscription expires in "<<days<<" days. Renew now and save 10%"<<endl;
           }
+
+       if (days >=6 && days <= 10){
+        cout<<"Your subscription will expire soon. Renew now!"<<endl;
+       }
+
+       if (days == 11){
+        cout<<"You have an active subscription."<<endl;
+       }
           
          
          
