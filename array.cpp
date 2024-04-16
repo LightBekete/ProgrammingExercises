4d0b4144cfca4d751ab2b7acc27c5bc6e1979423
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-16 20:39:41 +0200

    first commint of a program that has an array of string.The array has been declared and initialised

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+ using namespace std;
+  
+  int main(){
+    //array declaration
+    int arraysize = 8;
+    string codeArray[arraysize];
+    codeArray[0] = "B123";
+    codeArray[1] = "C234";
+    codeArray[2] = "A345";
+    codeArray[3] = "C15";
+    codeArray[4] = "B177";
+    codeArray[5] = "G3003";
+    codeArray[6] = "C235";
+    codeArray[7] = "B179";
+
+    return 0;
+  }
