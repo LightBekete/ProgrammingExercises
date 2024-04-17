ee212d45c8a844c1cdaf8b878e32e9272dbd383e
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-17 14:13:01 +0200

    first commit of the program that a user has to enter an integer value between 5 and 10. Two conditions have been implemeted which are ignoring string input and terminating the loop

---
+++ intergerbetween5and10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+
+int main() {
+    int value;
+    bool loopcondition = true;
+
+    while (loopcondition) {
+        cout << "Enter an integer value between 5 and 10: "<<endl;
+        if (!(cin >> value)) { // Check if input is not an integer
+            cout << "Sorry, you entered an invalid input. Please try again." <<endl;
+            cin.clear(); // this is used to Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); //  this ignores invalid input and clears the buffer
+       } else {
+            loopcondition = false; // Exit the loop if input is valid and within the range
+        }
+    }
+
+    std::cout << "Your input value  (" << value << ") has been accepted." <<endl;
+
+    return 0;
+}
+
+
+    
