1f126f88b03c542bc88f57d07e056f9c0048f8eb
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-22 16:58:20 +0200

    second and final commit of Lab6DynamicArrayofDoubles.cpp

--- Lab6DynamicArrayofDoubles.cpp
+++ Lab6DynamicArrayofDoubles.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <limits>
 using namespace std;
  
@@ -7,30 +8,39 @@ using namespace std;
     cout<<"Enter the Row[] number between 1 and 3 only"<<endl;
     int numberOfRows;
     cin>>numberOfRows;
-    while(!numberOfRows || (  numberOfRows < 1 || numberOfRows > 3)){
+    
+    if(!numberOfRows || (  numberOfRows <= 0 || numberOfRows > 3)){
+        cout<<" wrong input. try again."<<endl;
+    }
+    while(!numberOfRows || (  numberOfRows <= 0 || numberOfRows > 3)){
           cin>>numberOfRows;
         if (!numberOfRows){
              cin.clear();
              cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"you have entereed an invalid input"<<endl;
         }
-        else if ( numberOfRows < 1 || numberOfRows > 3){
+        else if ( numberOfRows <=0 || numberOfRows > 3){
             cout<<"Enter a value between 1 and 3"<<endl;
         }
        
     }
 
     //getting number of columns of the array
+      cout<<"Enter the column[] number between 1 and 3 only"<<endl;
         int  numberOfColums;
         cin>>numberOfColums;
-    while(!numberOfColums || (  numberOfColums < 1 || numberOfColums > 3)){
+
+        if(!numberOfColums || (  numberOfColums <= 0 || numberOfColums > 3)){
+        cout<<" wrong input. try again."<<endl;
+    }
+    while(!numberOfColums || (  numberOfColums <=0 || numberOfColums > 3)){
           cin>>numberOfColums;
         if (!numberOfColums){
              cin.clear();
              cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"you have entereed an invalid input"<<endl;
         }
-        else if ( numberOfColums < 1 || numberOfColums > 3){
+        else if ( numberOfColums <=0 || numberOfColums > 3){
             cout<<"Enter a value between 1 and 3"<<endl;
         }
        
@@ -43,6 +53,28 @@ using namespace std;
      for (int i = 0; i < numberOfRows; i++ ){
         doubleArray[i] = new double[numberOfColums];
      }
+
+     // randon initialisation of the doubleArray
+     srand(time(0));
+     for (int i = 0; i < numberOfRows; i++){
+        for (int j = 0;j < numberOfColums; j++){
+            doubleArray[i][j] = ((rand()%1500)/8);
+        }
+     }
+
+     //printi the doubleArray Elements
+     for (int i = 0; i < numberOfRows; i++){
+        for (int j = 0; j < numberOfColums; j++){
+            cout<<doubleArray[i][j]<<" ";
+        }
+         cout<<""<<endl;
+     }
+     // Deallocating memory of the doubleArray
+     for(int i = 0; i < numberOfRows; i++){
+        delete [] doubleArray[i];
+     }
+     delete [] doubleArray;
+     doubleArray =nullptr;
 
     
 
