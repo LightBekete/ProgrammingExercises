b1d69aed9a82f887bf17282a1a3226d194a4e969
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-04-29 15:48:57 +0200

    commit for swap numbers program

---
+++ swapnumberswithPointers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int swapNumbers(int* newVArA , int* newVarB);
+
+   int main (){
+     int varA = 25;
+     int varB = 100;
+
+     int* refVarA = &varA;
+     int* refVArB = &varB;
+       cout<<"VarA before swap "<<varA<<endl;
+       cout<<"VarB before swap "<<varB<<endl;
+       swapNumbers( refVarA , refVArB);
+
+      cout<<"VarA after swap "<<varA<<endl;
+      cout<<"VarB after swap "<<varB<<endl;
+
+    
+      
+
+
+    return 0;
+   }
+   int swapNumbers(int* newVarA , int* newVarB){
+    int tempVar = *newVarA;
+    *newVarA = *newVarB;
+    *newVarB =tempVar;
+    return *newVarA;
+    return *newVarB;
+
+      
+    delete newVarA;
+      newVarA = nullptr;
+      delete newVarB;
+      newVarB = nullptr;
+
+   }
