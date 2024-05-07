9ee684589abf10719a642aa07fa6a8f0eecf7afd
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-05-07 15:35:29 +0200

    this the box project that has the user th + operator overloading,i t has been fully implemented and tested

---
+++ LabTask8/Box.h
@@ -0,0 +1,25 @@
+#ifndef Box_h
+#define Box_h
+#include <iostream>
+using namespace std;
+
+class Box {
+public:
+    Box();
+    Box(const double newLength, const double newBreadth, const double newHeight);
+    ~Box();
+    double GetVolume();
+    void SetLength(double len);
+    void SetBreadth(double bre);
+    void SetHeight(double hei);
+    Box operator+(const Box& b);
+
+private:
+    double length;
+    double breadth;
+    double height;
+};
+
+#endif
+ 
+ 
