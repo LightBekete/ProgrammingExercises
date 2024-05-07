3ff962d7e9a1770c97c20781f6702af1b947a934
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-05-07 15:38:03 +0200

    this the person project the + overload operator habs been declared and defined,,and the class implemetation of person with the + overloading operator has been implemeted in the main.cpp

---
+++ LabTask8/Person/Person.cpp
@@ -0,0 +1,20 @@
+#include "Person.h"
+#include <iostream>
+
+Person::Person()  {
+    mWeight = 0;
+    mFirstName = ""; 
+    mAge = 0;
+}
+
+Person::Person(string newName, float newWeight)  {
+    mWeight = newWeight;
+    mFirstName = newName;
+    mAge = 0;
+}
+
+Person::~Person() {}
+
+float Person::operator+(const Person& otherPerson) {
+    return mWeight + otherPerson.mWeight;
+}
