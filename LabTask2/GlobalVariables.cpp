22591929b08d0e523110d25199541d45f98c00e0
LightBekete <bsc-com-ne-07-21@unima.ac.mw>
2024-05-10 01:07:07 +0200

    lab task 2 commit of datatypes

---
+++ LabTask2/GlobalVariables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//Global varibale declaration
+int g;
+
+int main(){
+    //Local variables declaration
+    int a, b;
+    //actual initialisation
+    a=10;
+    b=20;
+    g=a+b;
+    return 0;
+}
